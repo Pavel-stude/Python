3bb222d42850a902913c3d3d7ab5bd0f22c8af61
Pavel-stude <kremar555@gmail.com>
2026-02-08 03:49:39 +0900

    Add files via upload

---
+++ lesson_6_task_3.py
@@ -0,0 +1,20 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+waiter = WebDriverWait(driver, 40, 0.01)
+
+driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
+
+waiter.until(
+    EC.presence_of_element_located( (By.CSS_SELECTOR, "#landscape") )
+)
+
+src = driver.find_element(By.CSS_SELECTOR, "#award").get_attribute("src")
+print(src)
