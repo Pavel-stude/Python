49fccfbeec7336e257d07b68a79bfab03ea6d14d
Pavel-stude <kremar555@gmail.com>
2026-03-01 20:46:24 +0900

    Add files via upload

--- 06K_lesson/test_02_calc.py
+++ 06K_lesson/test_02_calc.py
@@ -1,27 +1,40 @@
 import pytest
-from selenium.webdriver.common.by import By
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    service = ChromeService(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service)
     yield driver
     driver.quit()
 
 
 def test_calculator(driver):
-    driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
-    driver.find_element(By.CSS_SELECTOR, "#delay").clear()
-    driver.find_element(By.CSS_SELECTOR, "#delay").send_keys(45)
+    driver.get(
+        "https://bonigarcia.dev/"
+        "selenium-webdriver-java/slow-calculator.html"
+    )
+
+    delay = driver.find_element(By.CSS_SELECTOR, "#delay")
+    delay.clear()
+    delay.send_keys("45")
+
     driver.find_element(By.XPATH, "//span[text()='7']").click()
     driver.find_element(By.XPATH, "//span[text()='+']").click()
     driver.find_element(By.XPATH, "//span[text()='8']").click()
     driver.find_element(By.XPATH, "//span[text()='=']").click()
 
-    res = WebDriverWait(driver, 50).until(
-    EC.text_to_be_present_in_element( (By.CSS_SELECTOR, "div.screen"), "15" ) )
-    assert res
+    result = WebDriverWait(driver, 50).until(
+        EC.text_to_be_present_in_element(
+            (By.CSS_SELECTOR, "div.screen"),
+            "15",
+        )
+    )
+
+    assert result
